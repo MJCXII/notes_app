bcf16f48610601408b7b6003fdb53fa83510214a
olga <itsglimerry@gmail.com>
2021-03-16 11:15:55 +0000

    Notes appearing if theyre in notebook variable on page load.

--- src/interface.js
+++ src/interface.js
@@ -1,3 +1,54 @@
-class Interface {
-  
-}
+
+
+
+window.onload = showAllArticles();
+
+function showAllArticles(){
+
+      for(let i = 0; i < notebook.length ; i++){
+        var oneNote = document.createElement("a")
+        var string = notebook[i].content
+
+        if(string.length > 20) {
+          string = string.substring(0,20) + '...' ;
+        }
+        oneNote.setAttribute("href",'#' + `${string}`)
+        oneNote.innerHTML = string
+        document.getElementById("box").appendChild(oneNote)
+      }
+
+   }
+
+
+  // makeUrlChangeShowArticleForCurrentPage();
+  //
+  //     function makeUrlChangeShowArticleForCurrentPage() {
+  //       window.addEventListener("hashchange", showArticleForCurrentPage);
+  //     };
+  //
+  //     function showArticleForCurrentPage() {
+  //       let article = getArticleFromUrl(window.location)
+  //
+  //       if (article) {
+  //       showArticle(article);
+  //       }
+  //       else {
+  //           document.getElementById('newsWrap').innerHTML = ""
+  //         showAllArticles();
+  //       }
+  //     };
+  //
+  //     function getArticleFromUrl(location) {
+  //       return location.hash.split("#")[1];
+  //     };
+  //
+  //     function showArticle(article) {
+  //
+  //
+  //       fetch("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + "https://www.theguardian.com/" + article )
+  //         .then(res => res.json())
+  //         .then(summary => {
+  //           console.log(summary)
+  //           document.getElementById('newsWrap').innerHTML = summary.sentences
+  //         });
+  //     };
--- src/note.js
+++ src/note.js
@@ -1,3 +1,15 @@
-class Notes {
+class Note {
+  constructor(title, content) {
+    this.title = title;
+    this.content = content;
+  }
+
+  title() {
+    return this.title;
+  }
+
+  content() {
+    return this.content;
+  }
 
 }
